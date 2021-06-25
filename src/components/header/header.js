0d8845625a52502ea27b040cb1f3b26c642c1036
hjpunzalan <hj.punzalan@hotmail.com>
2021-06-25 23:52:30 +0800

    navbar added - no styling

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,17 +1,22 @@
 import * as React from "react"
 import PropTypes from "prop-types"
+import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import * as classes from "./header.module.scss"
 
 const Header = () => (
-  <header>
+  <header className={classes.header}>
  <StaticImage
       className={classes.logo}
       src="../../images/logo-black-whitebg.png"
       formats={["AUTO", "WEBP", "AVIF"]}
       alt="Marine Archipelago"
     />
+    <div className={classes.links}>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact us</Link>
+    </div>
 </header>
 )
 
