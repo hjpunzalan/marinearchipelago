4a23fe6d246ca832b58bd02981a3e25156d41f99
hjpunzalan <hj.punzalan@hotmail.com>
2021-06-28 23:42:21 +0800

    header component bg and desc added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,20 +6,28 @@ import { StaticImage } from "gatsby-plugin-image"
 import * as classes from "./header.module.scss"
 
 const Header = () => (
-  <header>
+  <header className={classes.header}>
     <nav className={classes.nav}>
  <StaticImage
       className={classes.logo}
-      src="../../images/logo-black-whitebg.png"
+      src="../../images/logo-white-transparentbg.png"
       formats={["AUTO", "WEBP", "AVIF"]}
       alt="Marine Archipelago"
     />
     <div className={classes.links}>
       <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <Link to="/about">About us</Link>
       <Link to="/contact">Contact us</Link>
     </div>
     </nav>
+    <div className={classes.desc}>
+      <h1>
+        Discover world-class marine and subsea expertise
+      </h1>
+      <button className={classes.cta}>
+        Learn more
+      </button>
+    </div>
 </header>
 )
 
