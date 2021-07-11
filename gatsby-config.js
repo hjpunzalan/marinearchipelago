21ad8ebd3fbc0235057c2c5fac9e6e790fe2a805
hjpunzalan <hj.punzalan@hotmail.com>
2021-07-11 23:42:11 +0800

    add formspree

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
-    `@formspree/react`
+    `@formspree/react`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
