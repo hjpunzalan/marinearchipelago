17a7f7a2ff12936b56416aea4d59da179bec4257
hjpunzalan <hj.punzalan@hotmail.com>
2021-07-26 20:44:25 +0800

    contact page complete

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,45 +1,156 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React, { useState } from "react";
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Container from '../components/container/container'
+import Nav from '../components/navigation/nav'
+
+import "../styles/_main.scss";
+import * as classes from "../styles/pages/contact/contact.module.scss"
+
 
 import { useForm, ValidationError } from '@formspree/react';
 
-function Contact() {
+const ContactPage = () => {
+  const [nav, setNav] = useState(false);
+  const toggleNav = () => setNav(!nav)
+
+  // Formspree
   const [state, handleSubmit] = useForm("xbjkvkgb");
-  if (state.succeeded) {
-      return <p>Thanks for joining!</p>;
-  }
+  const [inputs, setInputs] = useState({
+    fullName:"",
+    email:"",
+    message:""
+  })
+
+  const handleChange = event => {
+    event.persist()
+    setInputs(prev => ({
+      ...prev,
+      [event.target.id]: event.target.value
+    }));
+  };
+
+
+
+
+
   return (
-      <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
-      <textarea
-        id="message"
-        name="message"
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      />
-      <button type="submit" disabled={state.submitting}>
-        Submit
-      </button>
-    </form>
+    <Layout mainClassName={classes.main} nav={nav}>
+      <Seo title="Archipelago Marine" description="Contact us" />
+      
+     <header className={classes.header}>
+      <Container>
+          <Nav nav={nav} toggleNav={toggleNav} />
+       <h1 className={classes.title}>
+        Contact us
+        </h1>
+      </Container>
+      </header>
+
+      
+      <Container>
+        <section className={classes.contact} style={{flexDirection: state.succeeded? "column": "flex"}}>
+          <div>
+          {
+            state.succeeded ? <p className={classes.thankMessage}>Thanks for your interest in Marine Archipelago. We will reach out to you shortly. Cheers!</p> :
+            <form onSubmit={handleSubmit}>
+                  <h2>Send us a message</h2>
+                  <div className={classes.container}>
+                  <div className={classes.customerInfo}>
+                <label htmlFor="firstName">
+                  Full Name:
+                </label>
+                <input
+                  id="fullName"
+                  type="text"
+                  name="Full Name:"
+                  onChange={handleChange}
+                    value={inputs.firstName}
+                    required
+                  />
+                  <ValidationError
+                  prefix="First Name"
+                  field="firstName"
+                  errors={state.errors}
+                  />
+                  <ValidationError
+                  prefix="Last Name"
+                  field="lastName"
+                  errors={state.errors}
+                  />
+                <label htmlFor="email">
+                  Email Address:
+                </label>
+                <input
+                  id="email"
+                  type="email"
+                    name="Email address:"
+                    onChange={handleChange}
+                    value={inputs.email}
+                    required
+                />
+                <ValidationError
+                  prefix="Email"
+                  field="email"
+                  errors={state.errors}
+                  />
+                    </div>
+                    
+
+
+
+            <div className={classes.messageAndSubmit}>
+                  <label className={classes.message} htmlFor="message">
+                  Write your message:
+                </label>
+                <textarea
+                  id="message"
+                  name="Message:"
+                  onChange={handleChange}
+                    value={inputs.message}
+                    required
+                />
+                <ValidationError
+                  prefix="Message"
+                  field="message"
+                  errors={state.errors}
+                  />
+                  <div>
+                <button type="submit" disabled={state.submitting}>
+                  {state.submitting ? "Please wait" : "Submit"}
+                    </button>
+                    </div>
+                 </div>
+               </div>
+            </form>
+            }
+          </div>
+
+          <div className={classes.companyInfo}>
+            <div>
+            <h3>Email</h3>
+            <p>admin@marinearchiepelago.com.au</p>
+            <h3>Address</h3>
+            <p>
+              1103 Lambden Rd, Karratha Industrial Estate WA 6714
+            </p>
+            </div>
+          <iframe
+            title="company location"
+            className={classes.map}
+            width="300"
+            height="450"
+            placeholder= "BLURRED"
+            loading="lazy"
+            allowfullscreen
+            src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJLezZghRr9isRaFFMS1rIJiM&key=AIzaSyA494M6sS0tEAq8JvvEH8d3R685xEhij48"></iframe>
+            </div>
+    </section>
+    </Container>
+  </Layout>
+     
   );
 }
 
-export default Contact
+export default ContactPage
