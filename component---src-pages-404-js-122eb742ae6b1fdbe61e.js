6b7c6ce49d4d294c37af42ea920841c55a80e6d4
hjpunzalan <hj.punzalan@hotmail.com>
2021-07-27 02:04:47 +0800

    Updates

---
+++ component---src-pages-404-js-122eb742ae6b1fdbe61e.js
@@ -0,0 +1,2 @@
+(self.webpackChunkarchipelago_marine=self.webpackChunkarchipelago_marine||[]).push([[883],{9616:function(e,n,a){"use strict";a.r(n);var u=a(7294),c=a(5444);n.default=function(){return(0,u.useEffect)((function(){(0,c.navigate)("/")}),[]),null}}}]);
+//# sourceMappingURL=component---src-pages-404-js-122eb742ae6b1fdbe61e.js.map
