103453c94a42fc868efc097b025d797cedd47a70
hjpunzalan <hj.punzalan@hotmail.com>
2021-09-28 00:55:10 +0800

    add public folder to cpanel branch

---
+++ public/component---src-pages-404-js-e679fbb3197db47fab28.js
@@ -0,0 +1,2 @@
+(self.webpackChunkmarine_archipelago=self.webpackChunkmarine_archipelago||[]).push([[883],{9616:function(e,n,a){"use strict";a.r(n);var u=a(7294),c=a(5444);n.default=function(){return(0,u.useEffect)((function(){(0,c.navigate)("/")}),[]),null}}}]);
+//# sourceMappingURL=component---src-pages-404-js-e679fbb3197db47fab28.js.map
